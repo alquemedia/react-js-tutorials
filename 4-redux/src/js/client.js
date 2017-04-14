040e2faeec14bb05e44ccfa9704965e69900d68d
Alquemedia S.A.S <david@alquemedia.com>
2017-04-14 11:33:27 -0500

    Learning Redux, basic structure, reducers, store, middleware [1]

--- 4-redux/src/js/client.js
+++ 4-redux/src/js/client.js
@@ -1,43 +1,53 @@
-import { applyMiddleware, createStore } from "redux";
+import { applyMiddleware, combineReducers, createStore } from "redux";
 import axios from "axios";
 import logger from "redux-logger";
 import thunk from "redux-thunk";
-import promise from "redux-promise-middleware";
 
-const initialState = {
-  fetching: false,
-  fetched: false,
-  users: [],
-  error: null,
+const userReducer = (state={},action) => {
+    switch (action.type ){
+        case "CHANGE_NAME":
+            state = {...state,name: action.payload};
+            break;
+        case "CHANGE_AGE":
+            state = {...state,age: action.payload};
+            break;
+        case "PROVOKE_ERROR":
+                throw new Error(action.payload);
+            break;
+    }
+    return state;
 };
 
-const reducer = (state=initialState, action) => {
-  switch (action.type) {
-    case "FETCH_USERS_PENDING": {
-      return {...state, fetching: true}
-      break;
-    }
-    case "FETCH_USERS_REJECTED": {
-      return {...state, fetching: false, error: action.payload}
-      break;
-    }
-    case "FETCH_USERS_FULFILLED": {
-      return {
-        ...state,
-        fetching: false,
-        fetched: true,
-        users: action.payload,
-      }
-      break;
-    }
-  }
-  return state
-}
+const tweetsReducer = (state=[],actions) => {
+    return state;
+};
 
-const middleware = applyMiddleware(promise(), thunk, logger())
-const store = createStore(reducer, middleware)
+const reducers = combineReducers({
+    user: userReducer,
+    tweets: tweetsReducer
+});
 
-store.dispatch({
-  type: "FETCH_USERS",
-  payload: axios.get("http://rest.learncode.academy/api/wstern/users")
-})
+
+const error = (store) => (next) => (action) => {
+    try {
+        next(action);
+    } catch (e){
+        console.log("Oops!",e);
+    }
+};
+
+const middleware = applyMiddleware(thunk,logger());
+
+const store = createStore(reducers,{},middleware);
+
+store.subscribe(()=>{
+    console.log("Store changed",store.getState());
+});
+
+store.dispatch((dispatch)=>{
+    dispatch({type:"FETCH_USERS_START"});
+    axios.get("http://rest.learncode.academy/api/wstern/users")
+        .then((response) => {
+            dispatch({type:"FETCH_USERS_RECEIVED"});
+        });
+});
